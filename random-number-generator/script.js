56a25a3ee10d9affd208d0561d8a1a04d3d18173
vee <iam.vee01@gmail.com>
2022-02-14 00:27:43 +0100

    Add function and loop to generate password from a string of letters and numbers

--- random-number-generator/script.js
+++ random-number-generator/script.js
@@ -4,13 +4,24 @@ const countRandom = document.querySelector('.count');
 const generateRandom = document.querySelector('.generate');
 
 generateRandom.addEventListener('click', (e) => {
-  const min = 1;
-  const max = 100;
-  //generate random between 1 and 100
-  countRandom.innerHTML = Math.floor(Math.random() * (max - min + 1) + min)
+  const letters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let password = "";
 
-  // countRandom.innerHTML = Math.min(1,2,3,4,0,-3,5,66,77,86,655,23);
+  //loop through total variable letters to generate a 10 letter password
+  for(let i = 1; i <= 5; i++) {
+    let generate = letters[Math.floor(Math.random() * (62 + 1) )];
+    password += generate;
+    countRandom.innerHTML = password;
+  }
 
-  console.log('Generate Random number');
+
+  // const min = 1;
+  // const max = 100;
+  // //generate random between 1 and 100
+  // countRandom.innerHTML = Math.floor(Math.random() * (max - min + 1) + min)
+
+  // // countRandom.innerHTML = Math.min(1,2,3,4,0,-3,5,66,77,86,655,23);
+
+  // console.log('Generate Random number');
 
 })
