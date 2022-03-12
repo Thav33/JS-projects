267f4e7d45fab7dbf00ba445cb2e22d77a9701be
vee <iam.vee01@gmail.com>
2022-03-12 23:06:49 +0100

    Add function to convert fahrenheit to kelvin

--- temperature-converter/script.js
+++ temperature-converter/script.js
@@ -1,10 +1,11 @@
 const kelvinValue = document.querySelector('#kelvin');
 const fahrenheitValue = document.querySelector('#fahrenheit');
 
+//function to convert kelvin to fahrenheit
 const convertKelvin = () => {
   //get the value of input 'kelvin' and convert to dataType Number
   const kelvinValue = +(document.querySelector('#kelvin').value);
-alert (typeof kelvinValue)
+
   //convert the value in kelvin to celcius by subtracting 273
   let celcius;
   celcius = kelvinValue - 273;
@@ -12,14 +13,41 @@ alert (typeof kelvinValue)
 
   //convert to fahrenheit
   let fahrenheit;
-  fahrenheit = celcius * (9/5) + 32;
+  // fahrenheit = celcius * (9/5) + 32;
+  fahrenheit = celcius * (9/5);
+  fahrenheit = fahrenheit + 32;
 
-  //convert decimals to whole number
-  fahrenheit = Math.floor(fahrenheit);
+  //convert to whole number
+  fahrenheit = Math.round(fahrenheit);
 
   // console.log(`the temperature is ${fahrenheit} degress fahrenheit`)
-  //display the value in fahrenheit in the 'fahrenheitValue' inputBox
+  //display the input value in fahrenheit in the 'fahrenheitValue' inputBox
   fahrenheitValue.value = fahrenheit;
-} 
-
+}
+//call to function event
 kelvinValue.addEventListener('input', convertKelvin)
+
+
+//function to convert fahrenheit to kelvin
+const convertFahrenheit = () => {
+    //get the value of input 'fahrenheit' and convert to dataType Number
+  const fahrenheitValue = +(document.querySelector('#fahrenheit').value);
+
+  let celcius;
+  celcius = fahrenheitValue - 32;
+
+  let kelvin;
+  // kelvin = celcius * (9/5) + 273;
+  kelvin = celcius * 5;
+  kelvin = kelvin / 9;
+  kelvin = kelvin + 273;
+
+  //convert decimal to whole number
+  kelvin = Math.round(kelvin)
+
+  //display the input value in kelvin in the 'kelvinValue' inputBox
+  kelvinValue.value = kelvin
+
+}
+//call to function event
+fahrenheitValue.addEventListener('input', convertFahrenheit)
