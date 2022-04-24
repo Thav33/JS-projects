57c4f311ef3819d4212fbb10567bc9c28a90253b
vee <iam.vee01@gmail.com>
2022-04-24 22:08:59 +0100

    Refactor codes for calculator app

--- calculator/script.js
+++ calculator/script.js
@@ -1,144 +1,88 @@
-let total = 0; 
-let num1 = ''; 
-let num2 = '';
-let operator = ''; 
-let equationExpression = '';
+const myCalculator = document.querySelector('.calculator');
+const myCalculatorKeys = [ 
+  ['1', '2', '3', '+'],
+  ['4', '5', '6', '-'],
+  ['7', '8', '9', '*'],
+  ['AC', '0', '=', '/']
+];
+const myCalculatorOperator = ['+', '-', '*', '/']
+let displayCalc;
 
-//get a hold of the btn values and save in variable
-let expressionScreen = document.querySelector('#calculatorScreen');
-let finalScoreScreen = document.querySelector('#finalScoreScreen');
-let calculatorDiv = document.querySelector('#click-calculator-btns')
+//function to display calculator keys on DOM load
+const calculate = () => {
+displayCalc = document.createElement('div')
+displayCalc.innerHTML = '0'
+displayCalc.classList.add('output')
+myCalculator.appendChild(displayCalc)
 
-let btnOne = document.querySelector('#one');
-let btnTwo = document.querySelector('#two');
-let btnThree = document.querySelector('#three');
-let btnFour = document.querySelector('#four');
-let btnFive = document.querySelector('#five');
-let btnSix = document.querySelector('#six');
-let btnSeven = document.querySelector('#seven');
-let btnEight = document.querySelector('#eight');
-let btnNine = document.querySelector('#nine');
-let btnZero = document.querySelector('#zero');
-let btnDot = document.querySelector('#decimal');
+// console.log(displayCalc)
 
-let btnPlus = document.querySelector('#plus');
-let btnTimes = document.querySelector('#times');
-let btnMinus = document.querySelector('#minus');
-let btnDivide = document.querySelector('#divide');
-let btnEqualSign = document.querySelector('#equal-sign');
-let btnClear = document.querySelector('#all-clear');
+//display calculator keys by looping through 'myCalculatorKeys'
+myCalculatorKeys.forEach((keys) => {
+  let div = document.createElement('div');
+  // div.classList.add('row');
 
-//function for btn one
-const one = () => {
-  //check if user hasn't press operator. if yes, value is assigned to num1 else assign to num2
-  (operator === '') ? num1 += '1' : num2 += '1';
-}
+//loop through the nested array to get each keys
+keys.forEach((key) => {
+  let btn = document.createElement('div')
+  btn.innerHTML = key
+  btn.classList.add('btn')
 
-const two = () => {
-  (operator === '') ? num1 += '2' : num2 += '2';
-}
+  //listen for the 'click' event on btn, then run the 'btnClicked' function
+  btn.addEventListener('click', btnClicked)
 
-const three = () => {
-  (operator === '') ? num1 += '3' : num2 += '3';
-}
-const four = () => {
-  (operator === '') ? num1 += '4' : num2 += '4';
-}
-const five = () => {
-  (operator === '') ? num1 += '5' : num2 += '5';
-}
-const six = () => {
-  (operator === '') ? num1 += '6' : num2 += '6';
-}
-const seven = () => {
-  (operator === '') ? num1 += '7' : num2 += '7';
-}
-const eight = () => {
-  (operator === '') ? num1 += '8' : num2 += '8';
-}
-const nine = () => {
-  (operator === '') ? num1 += '9' : num2 += '9';
-}
-const zero = () => {
-  (operator === '') ? num1 += '0' : num2 += '0';
-}
-const dot = () => {
-  (operator === '') ? num1 += '.' : num2 += '.';
+  div.appendChild(btn)
+  // console.log(btn)
+})
+
+myCalculator.appendChild(div)
+
+})
 }
 
-//functions for add sign
-const add = () => {
-  operator = '+';
-}
-//functions for multiply sign
-const multiply = () => {
-  operator = '*';
-}
-//functions for subtract sign
-const subtract = () => {
-  operator = '-';
-}
-//functions for divide sign
-const divide = () => {
-  operator = '/';
-}
+//function to run when 'btn' is clicked
+function btnClicked(){
+  let keyValue = this.innerHTML;
+  let keyCalculation = displayCalc.innerHTML
 
-//function to check with operator btn clicked. Then perform the operation based on the operator.
-const equal = () => {
+  if(keyCalculation === '0') { keyCalculation = ''}
 
-  //if the + operator is clicked, perform the addition operation
-  (operator === '+') ? total = +(num1) + +(num2) :
-  //if the * operator is clicked, perform the multiply operation
-  (operator === '*') ? total = +(num1) * +(num2) :
-  //if the - operator is clicked, perform the subtraction operation
-  (operator === '-') ? total = +(num1) - +(num2) :
-  //if the above 3 conditions are false, perform the division operation
-  total = +(num1) / +(num2);
-   
-  //display the given result based on the conditions.
-  finalScoreScreen.innerHTML = total;
-}
+  //condition to check if '=' is clicked. if true, evaluate expression
+  if(keyValue === '=') {
+    keyCalculation = eval(keyCalculation)
+  } else {
+    let lastKeyValue = keyCalculation.substring(0, keyCalculation.length - 1)
 
-//function to display whatever button is clicked
-const display = () => {
-  equationExpression = num1 + operator + num2
-  expressionScreen.innerText = equationExpression;
-}
+    //evaluate the expression
+    if(myCalculatorOperator.includes(keyValue)){
+      if(myCalculatorOperator.includes(lastKeyValue)){
+        keyCalculation = keyCalculation.substring(0, keyCalculation.length - 1) 
+      } else {
+        keyCalculation = eval(keyCalculation);
+      }
+    }
+  
+    keyCalculation += keyValue
+    // console.log(keyValue.innerHTML)
+  }
 
-//function to clear screen
-const clearCalculatorScreen = () => {
+  //condition to check if 'AC' was clicked. if true, clear expression.
+  if(keyValue === 'AC'){
+    keyCalculation = 0
+  }
+  
+  //display expression on calculator screen
+  displayCalc.innerText =  keyCalculation;
 
-  //if user click the clear btn, all variable should return to default
-  num1 = '';
-  num2 = '';
-  operator = '';
-  total = 0;
 
-  //update the screen to reflect the current values
-  finalScoreScreen.innerHTML = total;
-  // expressionScreen.innerText = ' ';
 }
 
 
+//load calculator contents as soon as the DOM is loaded
+document.addEventListener('DOMContentLoaded', calculate)
 
-calculatorDiv.addEventListener('click', display);
 
-btnOne.addEventListener('click', one);
-btnTwo.addEventListener('click', two);
-btnThree.addEventListener('click', three);
-btnFour.addEventListener('click', four);
-btnFive.addEventListener('click', five);
-btnSix.addEventListener('click', six);
-btnSeven.addEventListener('click', seven);
-btnEight.addEventListener('click', eight);
-btnNine.addEventListener('click', nine);
-btnZero.addEventListener('click', zero);
-btnDot.addEventListener('click', dot);
 
-btnPlus.addEventListener('click', add);
-btnTimes.addEventListener('click', multiply);
-btnMinus.addEventListener('click', subtract);
-btnDivide.addEventListener('click', divide);
-btnEqualSign.addEventListener('click', equal);
-btnClear.addEventListener('click', clearCalculatorScreen)
+
+
 
